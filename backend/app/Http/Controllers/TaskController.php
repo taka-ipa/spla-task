ede66c61b512495a2815ba1db12fc689eb2b9615
taka-ipa <taka.ipa1110@gmail.com>
2025-07-05 23:11:13 +0900

    feat: TaskControllerとapi/tasksルートを追加（ダミーデータ返却）

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            ['id' => 1, 'title' => 'エイム練習', 'status' => '○'],
+            ['id' => 2, 'title' => '立ち回り反省', 'status' => '△'],
+            ['id' => 3, 'title' => '打開反省', 'status' => '×'],
+        ]);
+    }
+}
