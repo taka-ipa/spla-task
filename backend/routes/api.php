c7ad42c4d9e1ebb44ee5b2ac9d03c6a2fc6b825e
taka-ipa <taka.ipa1110@gmail.com>
2025-07-09 23:47:45 +0900

    laravel breezeの導入

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
 
-Route::get('/tasks', [TaskController::class, 'index']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
