88944c5905383787d6274c1c14fb1f55ea84337c
taka-ipa <taka.ipa1110@gmail.com>
2025-07-14 23:42:03 +0900

    laravel breezeの初期セットアップを行いました。

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
